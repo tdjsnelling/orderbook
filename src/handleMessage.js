b522346f053276bd8800dc154e730ef1defbbed2
Tom Snelling <tomsnelling8@gmail.com>
2021-08-27 17:57:26 +0100

    basic push to redis

--- src/handleMessage.js
+++ src/handleMessage.js
@@ -1,6 +1,6 @@
-const handleMessage = (uid, buf, OrderMessage) => {
-  const decoded = OrderMessage.decode(buf)
-  const message = OrderMessage.toObject(decoded)
+const handleMessage = async ({ uid, buf, proto, redisClient }) => {
+  const decoded = proto.decode(buf)
+  const message = proto.toObject(decoded)
 
   if (uid !== message.uid) {
     console.log('message rejected: uid mismatch')
@@ -8,6 +8,13 @@ const handleMessage = (uid, buf, OrderMessage) => {
   }
 
   console.log(`message: ${uid}: ${JSON.stringify(message)}`)
+
+  const { side, symbol, price } = message
+
+  await redisClient.RPUSH(
+    `${side}:${symbol}@${price}`,
+    JSON.stringify({ uid, ts: Date.now() })
+  )
 }
 
 export default handleMessage
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import { WebSocketServer } from 'ws'
 import dotenv from 'dotenv'
 import protobuf from 'protobufjs'
+import { createClient } from 'redis'
 
 import handleMessage from './handleMessage'
+
+// iife
 ;(async () => {
   dotenv.config()
 
   const proto = await protobuf.load('src/proto/order.proto')
   const OrderMessage = proto.lookupType('orderbook.Order')
+
+  const redisClient = createClient({
+    socket: {
+      url: process.env.REDIS_URL,
+    },
+  })
+
+  await redisClient.connect()
 
   const clients = {}
   const wss = new WebSocketServer({ port: process.env.PORT || 9696 })
@@ -16,7 +27,12 @@ import handleMessage from './handleMessage'
     const [, uid] = req.url.split('/')
 
     ws.on('message', (message) => {
-      const reply = handleMessage(uid, message, OrderMessage)
+      const reply = handleMessage({
+        uid,
+        buf: message,
+        proto: OrderMessage,
+        redisClient,
+      })
       if (reply) ws.send(JSON.stringify(reply))
     })
 
