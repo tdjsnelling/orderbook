306c1e3c2394396847d82800043b30a731ed9765
Tom Snelling <tomsnelling8@gmail.com>
2021-09-03 17:57:02 +0100

    improved message handling

--- src/handleMessage.js
+++ src/handleMessage.js
@@ -1,20 +1,57 @@
-const handleMessage = async ({ uid, buf, proto, redisClient }) => {
-  const decoded = proto.decode(buf)
-  const message = proto.toObject(decoded)
+import protobuf from 'protobufjs'
+
+const createMessageHandler = (uid, redisClient) => async (rawMessage) => {
+  const json = JSON.parse(rawMessage)
+  const { type, data } = json
+
+  const proto = await protobuf.load('src/proto/order.proto')
+
+  console.log(`message:${uid}:${type}`)
+
+  switch (type) {
+    case 'order':
+      return await handleOrder({ data, uid, proto, redisClient })
+    case 'query':
+      return await handleQuery({ data, uid, proto, redisClient })
+    default:
+      return { error: `Message type “${type}” unknown` }
+  }
+}
+
+const handleOrder = async ({ data, uid, proto, redisClient }) => {
+  const OrderMessage = proto.lookupType('orderbook.Order')
+
+  const buf = Buffer.from(data, 'base64')
+  const decoded = OrderMessage.decode(buf)
+  const message = OrderMessage.toObject(decoded)
 
   if (uid !== message.uid) {
     console.log('message rejected: uid mismatch')
-    return { error: 'User IDs do not match' }
+    return { type: 'order', error: 'User IDs do not match' }
   }
-
-  console.log(`message: ${uid}: ${JSON.stringify(message)}`)
 
   const { side, symbol, price } = message
 
-  await redisClient.RPUSH(
-    `${side}:${symbol}@${price}`,
-    JSON.stringify({ uid, ts: Date.now() })
-  )
+  const ts = Date.now()
+  const orderString = `${side}:${symbol}@${price}`
+  console.log(`order:${uid}:${orderString}`)
+
+  await redisClient.RPUSH(orderString, JSON.stringify({ uid, ts }))
+
+  return { type: 'order', message: `Order submitted at ${ts}` }
 }
 
-export default handleMessage
+const handleQuery = async ({ data, uid, proto, redisClient }) => {
+  const OrderMessage = proto.lookupType('orderbook.Query')
+
+  const buf = Buffer.from(data, 'base64')
+  const decoded = OrderMessage.decode(buf)
+  const message = OrderMessage.toObject(decoded)
+
+  if (uid !== message.uid) {
+    console.log('message rejected: uid mismatch')
+    return { type: 'order', error: 'User IDs do not match' }
+  }
+}
+
+export default createMessageHandler
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import { WebSocketServer } from 'ws'
 import dotenv from 'dotenv'
-import protobuf from 'protobufjs'
 import { createClient } from 'redis'
 
-import handleMessage from './handleMessage'
+import createMessageHandler from './handleMessage'
 
 // iife
 ;(async () => {
   dotenv.config()
-
-  const proto = await protobuf.load('src/proto/order.proto')
-  const OrderMessage = proto.lookupType('orderbook.Order')
 
   const redisClient = createClient({
     socket: {
@@ -26,21 +22,18 @@ import handleMessage from './handleMessage'
   wss.on('connection', (ws, req) => {
     const [, uid] = req.url.split('/')
 
-    ws.on('message', (message) => {
-      const reply = handleMessage({
-        uid,
-        buf: message,
-        proto: OrderMessage,
-        redisClient,
-      })
+    const handleMessage = createMessageHandler(uid, redisClient)
+
+    ws.on('message', async (message) => {
+      const reply = await handleMessage(message.toString())
       if (reply) ws.send(JSON.stringify(reply))
     })
 
     ws.on('close', () => {
-      console.log(`client disconnected: ${uid}`)
+      console.log(`disconnected:${uid}`)
     })
 
     clients[uid] = ws
-    console.log(`client connected: ${uid}`)
+    console.log(`connected:${uid}`)
   })
 })()
